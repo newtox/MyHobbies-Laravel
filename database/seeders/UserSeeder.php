42f2b37e59ae26c80192150bf64b0d5c0f797d79
Newtox <futzy261@gmail.com>
2024-10-14 16:13:04 +0200

    Added seperated seeding option

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,11 +16,23 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->count(50)->create()->each(function ($user) {
+        User::factory()->count(500)->create()->each(function ($user) {
             Hobby::factory()->count(random_int(1, 10))->create(['user_id' => $user->id])->each(function ($hobby) use ($user) {
                 $tag_ids = range(1, 8);
                 shuffle($tag_ids);
-                $relation = array_slice($tag_ids, 0, random_int(0, 8));
+
+                // Method to insert a user with only one tag for each hobby.
+                if ($hobby->id) {
+                    DB::table('hobby_tag')->insert([
+                       'hobby_id' => $hobby->id,
+                       'tag_id' => $tag_ids[0],
+                       'created_at' => now(),
+                       'updated_at' => now(),
+                    ]);
+                }
+
+                // Method to insert a user with different amount of tags for each hobby.
+/*                $relation = array_slice($tag_ids, 0, random_int(0, 8));
 
                 foreach ($relation as $tagId) {
                     if ($hobby->id) {
@@ -31,7 +43,7 @@ class UserSeeder extends Seeder
                             'updated_at' => now(),
                         ]);
                     }
-                }
+                }*/
             });
         });
 
